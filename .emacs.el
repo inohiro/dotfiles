718e39df9d2a66bd739b0080085a0788d2646f61
Hiroyuki Inoue <mammymax@gmail.com>
2012-05-08 13:58:18 +0900

    add powerline

--- .emacs.el
+++ .emacs.el
@@ -109,10 +109,6 @@
 (ruby-block-mode t)
 (setq ruby-block-highlight-toggle t)
 
-;; flymake-ruby
-;;;; (require 'flymake-ruby)
-;;;; (add-hook 'ruby-mode-hook 'flymake-ruby-load)
-
 ;; flymake for ruby
 (require 'flymake)
 ;; Invoke ruby with '-c' to get syntax checking
@@ -154,25 +150,129 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PowerLine
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun arrow-right-xpm (color1 color2)
+  "Return an XPM right arrow string representing."
+  (format "/* XPM */
+static char * arrow_right[] = {
+\"12 18 2 1\",
+\". c %s\",
+\"  c %s\",
+\".           \",
+\"..          \",
+\"...         \",
+\"....        \",
+\".....       \",
+\"......      \",
+\".......     \",
+\"........    \",
+\".........   \",
+\".........   \",
+\"........    \",
+\".......     \",
+\"......      \",
+\".....       \",
+a\"....        \",
+\"...         \",
+\"..          \",
+\".           \"};"  color1 color2))
+
+(defun arrow-left-xpm (color1 color2)
+  "Return an XPM right arrow string representing."
+  (format "/* XPM */
+static char * arrow_right[] = {
+\"12 18 2 1\",
+\". c %s\",
+\"  c %s\",
+\"           .\",
+\"          ..\",
+\"         ...\",
+\"        ....\",
+\"       .....\",
+\"      ......\",
+\"     .......\",
+\"    ........\",
+\"   .........\",
+\"   .........\",
+\"    ........\",
+\"     .......\",
+\"      ......\",
+\"       .....\",
+\"        ....\",
+\"         ...\",
+\"          ..\",
+\"           .\"};"  color2 color1))
+
+
+(defconst color1 "#FF6699")
+(defconst color3 "#CDC0B0")
+(defconst color2 "#FF0066")
+(defconst color4 "#CDC0B0")
+
+(defvar arrow-right-1 (create-image (arrow-right-xpm color1 color2) 'xpm t :ascent 'center))
+(defvar arrow-right-2 (create-image (arrow-right-xpm color2 "None") 'xpm t :ascent 'center))
+(defvar arrow-left-1  (create-image (arrow-left-xpm color2 color1) 'xpm t :ascent 'center))
+(defvar arrow-left-2  (create-image (arrow-left-xpm "None" color2) 'xpm t :ascent 'center))
+
+(setq-default mode-line-format
+ (list  '(:eval (concat (propertize " %b " 'face 'mode-line-color-1)
+                        (propertize " " 'display arrow-right-1)))
+        '(:eval (concat (propertize " %m " 'face 'mode-line-color-2)
+                        (propertize " " 'display arrow-right-2)))
+
+        ;; Justify right by filling with spaces to right fringe - 16
+        ;; (16 should be computed rahter than hardcoded)
+        '(:eval (propertize " " 'display '((space :align-to (- right-fringe 17)))))
+
+        '(:eval (concat (propertize " " 'display arrow-left-2)
+                        (propertize " %p " 'face 'mode-line-color-2)))
+        '(:eval (concat (propertize " " 'display arrow-left-1)
+                        (propertize "%4l:%2c  " 'face 'mode-line-color-1)))
+)) 
+
+(make-face 'mode-line-color-1)
+(set-face-attribute 'mode-line-color-1 nil
+                    :foreground "#fff"
+                    :background color1)
+
+(make-face 'mode-line-color-2)
+(set-face-attribute 'mode-line-color-2 nil
+                    :foreground "#fff"
+                    :background color2)
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#fff"
+                    :background color3
+                    :box nil)
+(set-face-attribute 'mode-line-inactive nil
+                    :foreground "#fff"
+                    :background color4)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Show Invisible Characters
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;(setq whitespace-style
-;;      '(tabs tab-mark spaces space-mark))
-;;(setq whitespace-space-regexp "\\(\x3000+\\)")
-;;(setq whitespace-display-mappings
-;;      '((space-mark ?\x3000 [?\□])
-;;        (tab-mark   ?\t   [?\xBB ?\t])
-;;        ))
-;;(require 'whitespace)
+;; (setq whitespace-style
+;;       '(tabs tab-mark spaces space-mark))
+;; (setq whitespace-space-regexp "\\(\x3000+\\)")
+;; (setq whitespace-display-mappings
+;;       '((space-mark ?\x3000 [?\□])
+;; 	(tab-mark   ?\t   [?\xBB ?\t])
+;; 	))
+
+;; (require 'whitespace)
+;; (global-whitespace-mode 1)
+;; (set-face-foreground 'whitespace-space "LightSlateGray")
+;; (set-face-background 'whitespace-space "DarkSlateGray")
+;; (set-face-foreground 'whitespace-tab "LightSlateGray")
+;; (set-face-background 'whitespace-tab "DarkSlateGray")
+
 ;;(global-whitespace-mode 1)
-;;(set-face-foreground 'whitespace-space "LightSlateGray")
-;;(set-face-background 'whitespace-space "DarkSlateGray")
-;;(set-face-foreground 'whitespace-tab "LightSlateGray")
-;;(set-face-background 'whitespace-tab "DarkSlateGray")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Ocaml support
+;; Ocaml Support
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (setq auto-mode-alist
