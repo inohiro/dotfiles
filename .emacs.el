ccdb56071526db983cf273e242a5b55e699656f5
Hiroyuki Inoue <mammymax@gmail.com>
2012-10-29 18:33:06 +0900

    add conf-mode to .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -49,6 +49,15 @@
 (setq linum-format "%3d ")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;; conf-mode (default)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . conf-mode))
+(add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.cnf$"   . conf-mode))
+(add-to-list 'auto-mode-alist '("security$"   . conf-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; MEMO
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
