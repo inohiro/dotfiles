681f2af705bfe47e6dacec0cc45a2762a676a2de
Hiroyuki Inoue <hiroyuki.inoue@treasure-data.com>
2026-08-10 13:31:10 -0700

    Revise emacs settings

--- .emacs.el
+++ .emacs.el
@@ -9,42 +9,20 @@
        (list (expand-file-name "/usr/local/share/emacs/site-lisp")) load-path))
 (setq load-path
       (append
-       (list (expand-file-name "/Users/inohiro/.emacs.d")) load-path))
-(setq load-path
-      (append
-       (list (expand-file-name "/home/inohiro/.emacs.d")) load-path))
+       (list (expand-file-name "~/.emacs.d")) load-path))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; package.el
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; diff-hl.el
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (global-diff-hl-mode)
-;; (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
-;; (unless (window-system) (diff-hl-margin-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; set coding-system to UTF-8
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (set-keyboard-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; auto-complete-mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (require 'auto-complete-config)
-;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-;; (ac-config-default)
-
-;; (define-key ac-menu-map (kbd "C-n")   'ac-next)
-;; (define-key ac-menu-map (kbd "C-p")   'ac-previous)
-;; (define-key ac-menu-map (kbd "M-TAB") nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; set custom extension-mode combination
@@ -54,17 +32,6 @@
 (add-to-list 'auto-mode-alist '("\\.job\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.dig\\'" . yaml-mode))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; for Aspell (http://oku.edu.mie-u.ac.jp/~okumura/texwiki/?Aspell#g5b60c70)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq-default ispell-program-name "aspell") 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; for highlight-indentation (http://stackoverflow.com/questions/1587972/)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'highlight-indentation)
-(add-hook 'ruby-mode-hook 'highlight-indentation-mode)
-(set-face-background 'highlight-indentation-face "#6c71c4")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; emphasize current line
@@ -86,18 +53,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; line number
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'linum)
-(global-linum-mode)
-(setq linum-format "%3d ")
+(when (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; sparql-mode (https://github.com/ljos/sparql-mode)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(add-to-list 'load-path "/path/to/sparql-mode-dir")
-(autoload 'sparql-mode "sparql-mode.el"
-  "Major mode for editing SPARQL files" t)
-(add-to-list 'auto-mode-alist '("\\.sparql$" . sparql-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;; conf-mode (default)
@@ -107,57 +65,6 @@
 (add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . conf-mode))
 (add-to-list 'auto-mode-alist '("\\.cnf$"   . conf-mode))
 (add-to-list 'auto-mode-alist '("security$"   . conf-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Powerline (http://www.emacswiki.org/emacs-en/PowerLine)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(require 'powerline)
-;; (powerline-default)
-
-(setq-default mode-line-format
-              '("%e"
-                (:eval
-                 (let* ((active (eq (frame-selected-window) (selected-window)))
-                        (face1 (if active 'powerline-active1 'powerline-inactive1))
-                        (face2 (if active 'powerline-active2 'powerline-inactive2))
-                        (lhs (list
-                              (powerline-raw "%*" nil 'l)
-                              (powerline-buffer-size nil 'l)
-                              (powerline-buffer-id nil 'l)
-
-                              (powerline-raw " ")
-                              (powerline-arrow-right nil face1)
-
-                              (powerline-major-mode face1 'l)
-                              (powerline-minor-modes face1 'l)
-                              (powerline-raw mode-line-process face1 'l)
-
-                              (powerline-narrow face1 'l)
-
-                              (powerline-arrow-right face1 face2)
-
-                              (powerline-vc face2)
-                              ))
-                        (rhs (list
-                              (powerline-raw global-mode-string face2 'r)
-
-                              (powerline-arrow-left face2 face1)
-
-                              (powerline-raw "%4l" face1 'r)
-                              (powerline-raw ":" face1)
-                              (powerline-raw "%3c" face1 'r)
-
-                              (powerline-arrow-left face1 nil)
-                              (powerline-raw " ")
-
-                              (powerline-raw "%6p" nil 'r)
-
-                              (powerline-hud face2 face1))))
-                   (concat
-                    (powerline-render lhs)
-                    (powerline-fill face2 (powerline-width rhs))
-                    (powerline-render rhs))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; MEMO
@@ -178,57 +85,6 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Go support
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (require 'go-mode-load )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; n3 mode
-;; (https://github.com/kurtjx/n3-mode-for-emacs)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(add-to-list 'load-path "~/.emacs.d/n3-mode.el")
-(autoload 'n3-mode "n3-mode" "Major mode for OWL or N3 files" t)
-
-;; Turn on font lock when in n3 mode
-(add-hook 'n3-mode-hook
-          'turn-on-font-lock)
-
-(setq auto-mode-alist
-      (append
-       (list
-        '("\\.n3" . n3-mode)
-        '("\\.owl" . n3-mode))
-       auto-mode-alist))
-
-;; Replace {path} with the full path to n3-mode.el on your system.
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; C# support
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-(setq auto-mode-alist (cons '("\\.cs$" . csharp-mode) auto-mode-alist))
-
-;; NOTE: The version of cc-langs.el that came with my Emacs
-;; installation has the function c-filter-op defined only at compile
-;; time, but it's needed by csharp-mode at run-time. I hacked
-;; cc-langs.el and recompiled it to fix this.
-
-;; Patterns for finding Microsoft C# compiler error messages:
-(require 'compile)
-(push '("^\\(.*\\)(\\([0-9]+\\),\\([0-9]+\\)): error" 1 2 3 2) compilation-error-regexp-alist)
-(push '("^\\(.*\\)(\\([0-9]+\\),\\([0-9]+\\)): warning" 1 2 3 1) compilation-error-regexp-alist)
-
-;; Patterns for defining blocks to hide/show:
-(push '(csharp-mode
-	"\\(^\\s *#\\s *region\\b\\)\\|{"
-	"\\(^\\s *#\\s *endregion\\b\\)\\|}"
-	"/[*/]"
-	nil
-	hs-c-like-adjust-block-beginning)
-      hs-special-modes-alist)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ruby-mode
@@ -240,14 +96,6 @@
 
 (require 'ruby-electric)
 (add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
-
-;;
-;; ruby-block.el
-;;
-
-;; (require 'ruby-block)
-;; (ruby-block-mode t)
-;; (setq ruby-block-highlight-toggle t)
 
 (require 'ruby-end)
 
@@ -310,27 +158,6 @@
         )
       (setq count (1- count)))))
 
-;; ;;
-;; ;; rcodetools
-;; ;;
-;; (add-to-list 'load-path "~/.rbenv/versions/1.9.3-p429/lib/ruby/gems/1.9.1/gems/rcodetools-0.8.5.0/")
-;; (require 'rcodetools)
-;; (define-key ruby-mode-map (kbd "<C-return>") 'rct-complete-symbol)
-
-;;
-;; rsence
-;;
-(setq rsense-home "~/.emacs.d/rsense-0.3")
-(add-to-list 'load-path (concat rsense-home "/etc"))
-(require 'rsense)
-
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-             ;; .や::を入力直後から補完開始
-             (add-to-list 'ac-sources 'ac-source-rsense-method)
-             (add-to-list 'ac-sources 'ac-source-rsense-constant)
-             ;; C-x .で補完出来るようキーを設定
-             (define-key ruby-mode-map (kbd "C-x .") 'ac-complete-rsense)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; markdown mode
@@ -342,34 +169,6 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; js2-mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (autoload 'js2-mode "js2-mode/js2-mode" nil t)
-;; (require 'js2-mode)
-;; (add-to-list 'auto-mode-alist '("\\.\\(js\\|json\\)$" . js2-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Show Invisible Characters
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (setq whitespace-style
-;;       '(tabs tab-mark spaces space-mark))
-;; (setq whitespace-space-regexp "\\(\x3000+\\)")
-;; (setq whitespace-display-mappings
-;;       '((space-mark ?\x3000 [?\□])
-;; 	(tab-mark   ?\t   [?\xBB ?\t])
-;; 	))
-
-;; (require 'whitespace)
-;; (global-whitespace-mode 1)
-;; (set-face-foreground 'whitespace-space "LightSlateGray")
-;; (set-face-background 'whitespace-space "DarkSlateGray")
-;; (set-face-foreground 'whitespace-tab "LightSlateGray")
-;; (set-face-background 'whitespace-tab "DarkSlateGray")
-
-;;(global-whitespace-mode 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Use spaces instead of tabs
@@ -377,53 +176,6 @@
 
 ;; タブの代わりにスペースを使用
 (setq-default indent-tabs-mode nil)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Ocaml Support
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (setq auto-mode-alist
-;;      (cons '("\\.ml[iylp]?$" . caml-mode) auto-mode-alist))
-;; (autoload 'caml-mode "caml" "Major mode for editing Caml code." t)
-;; (autoload 'run-caml "inf-caml" "Run an inferior Caml Process." t)
-;; (if window-system(require 'caml-font))
-;; (setq inferior-caml-program "/usr/local/bin/ocaml")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; mmm-mode for jsp
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (require 'cl)
-;; (require 'mmm-auto)
-;; (require 'mmm-vars)
-
-;; (mmm-add-group 'jsp
-;;  `((jsp-comment
-;;     :submode text-mode
-;;     :face mmm-comment-submode-face
-;;     :front "<%--"
-;;     :back "--%>"
-;;     :insert ((?- jsp-comment nil @ "<%--" @ " " _ " " @ "--%>" @))
-;;     )
-;;    (jsp-code
-;;     :submode java
-;;     :match-face (("<%!" . mmm-declaration-submode-face)
-;;                  ("<%=" . mmm-output-submode-face)
-;;                  ("<%"  . mmm-code-submode-face))
-;;     :front "<%[!=]?"
-;;     :back "%>"
-;;     :match-name "jsp"
-;;     :insert ((?% jsp-code nil @ "<%" @ " " _ " " @ "%>" @)
-;;              (?! jsp-declaration nil @ "<%!" @ " " _ " " @ "%>" @)
-;;              (?= jsp-expression nil @ "<%=" @ " " _ " " @ "%>" @))
-;;     )
-;;    (jsp-directive
-;;     :submode text-mode
-;;     :face mmm-special-submode-face
-;;     :front "<%@"
-;;     :back "%>"
-;;     :insert ((?@ jsp-directive nil @ "<%@" @ " " _ " " @ "%>" @))
-;;     )))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -433,7 +185,7 @@
  '(large-file-warning-threshold nil)
  '(package-selected-packages
    (quote
-    (yaml-mode web-mode ruby-end ruby-electric rsense powerline highlight-indentation flymake-ruby))))
+    (yaml-mode web-mode ruby-end ruby-electric flymake-ruby))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
